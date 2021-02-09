462d2778c7b465198ae83c0387e1218a81f35bdd
arumugam555 <53862453+arumugam555@users.noreply.github.com>
2021-02-09 17:40:20 +0530

    new line added in newfile.java

--- newfile.java
+++ newfile.java
@@ -2,5 +2,6 @@ class Simpleprogram{
     public static void main(String args[]){  
      System.out.println("Hello this is my first git file");
      System.out.println("New line adding");  
+     System.out.println("Another New line adding"); 
     }  
 }  
