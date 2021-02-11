85f4ef19076cd4c4746343e953367309865c41fb
karti555 <arumugasamy291193@gmail.com>
2021-02-11 09:04:54 +0000

    demo1 file of branch1 is modified

--- demo1.java
+++ demo1.java
@@ -2,7 +2,7 @@ class demo()
 {
     public static void main (String args[])
     {
-      System.out.println("The Demo of Git Branching function");
+      System.out.println("this is a demo file");
       System.out.println("This is the secondline of Demo1 File");
     }
  }
