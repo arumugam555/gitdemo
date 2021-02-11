a9b97d2fe1b269bff3a0b69f41e12ba275542221
karti555 <karti555@ip-172-31-36-100.us-east-2.compute.internal>
2021-02-11 08:49:52 +0000

    Added new line to demo1 from Branch1

---
+++ demo1.java
@@ -0,0 +1,9 @@
+class demo()
+{
+    public static void main (String args[])
+    {
+      System.out.println("The Demo of Git Branching function");
+      System.out.println("This is the secondline of Demo1 File");
+    }
+ }
+© 2021 GitHub, Inc.
